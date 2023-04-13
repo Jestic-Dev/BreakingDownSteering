6dae55773e64c17cede835999421cf4061ea150f
Christian Christiansen <60815717+Jestic-Dev@users.noreply.github.com>
2023-04-13 22:23:49 +0200

    Update to Spawnable objects
    Has a area it can spawn within
    Spawn an selected prefab
    Can fire toward a selected direction

--- Assets/ObjectSpawner.cs
+++ Assets/ObjectSpawner.cs
@@ -1,46 +1,83 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-/*
-To use this script, 
-simply attach it to a game object in your scene and assign the objectToSpawn and spawnPoint variables in the inspector.
-You can also adjust the spawnInterval to change the time interval between spawn
-
-""
-*/
-
 public class ObjectSpawner : MonoBehaviour
 {
-    public GameObject objectToSpawn;
-    public SpawnArea spawnBounds;
-    public float spawnInterval = 1f;
-    public float spawnRange = 5f;
-    public float lifeDuration = 5f;
-    public float overlapRadius = 0.5f;
-    private float nextSpawnTime;
+    public GameObject objectPrefab; // The prefab of the object to be spawned
+    public Transform player; // The transform of the player
+    public Vector3 spawnAreaSize; // The size of the spawn area
+    public float spawnInterval = 1f; // The interval between spawns
+    public float moveSpeed = 10f; // The speed at which the objects move
+    public float avoidDistance = 5f; // The minimum distance between objects
+    public float destroyRange = 50f; // The maximum range at which objects are destroyed
+    public Vector3 flyDirection = Vector3.right; // The direction in which the objects should fly
 
-    private void Update()
+    private List<GameObject> spawnedObjects = new List<GameObject>();
+
+    void Start()
     {
-        if (Time.time >= nextSpawnTime)
+        StartCoroutine(SpawnObjects());
+    }
+
+    IEnumerator SpawnObjects()
+    {
+        while (true)
         {
-            SpawnObject();
-            nextSpawnTime = Time.time + spawnInterval;
+            Vector3 spawnPosition = GetRandomSpawnPosition() + transform.position;
+            Quaternion spawnRotation = Quaternion.Euler(0f, Random.Range(0f, 360f), 0f); // generate a random rotation around the Y axis
+            GameObject newObject = Instantiate(objectPrefab, spawnPosition, spawnRotation); // set the rotation of the new object to the random rotation
+            newObject.transform.rotation = Quaternion.LookRotation(flyDirection);
+            spawnedObjects.Add(newObject);
+
+            yield return new WaitForSeconds(spawnInterval);
         }
     }
 
-    private void SpawnObject()
+    Vector3 GetRandomSpawnPosition()
     {
-        Vector3 randomPos = spawnBounds.GetRandomPointInBounds();
-        bool isOverlapping = Physics.CheckSphere(randomPos, overlapRadius);
+        Vector3 spawnBoxMin = -spawnAreaSize / 2f;
+        Vector3 spawnBoxMax = spawnAreaSize / 2f;
 
-        while (!spawnBounds.bounds.Contains(randomPos) || isOverlapping)
+        int attempts = 10;
+        while (attempts-- > 0)
         {
-            randomPos = spawnBounds.GetRandomPointInBounds();
-            isOverlapping = Physics.CheckSphere(randomPos, overlapRadius);
+            Vector3 randomPosition = new Vector3(Random.Range(spawnBoxMin.x, spawnBoxMax.x),
+                                                 Random.Range(spawnBoxMin.y, spawnBoxMax.y),
+                                                 Random.Range(spawnBoxMin.z, spawnBoxMax.z));
+
+            bool overlap = false;
+            foreach (GameObject obj in spawnedObjects)
+            {
+                if (Vector3.Distance(randomPosition, obj.transform.position) < avoidDistance)
+                {
+                    overlap = true;
+                    break;
+                }
+            }
+
+            if (!overlap)
+            {
+                return randomPosition;
+            }
         }
 
-        GameObject newObject = Instantiate(objectToSpawn, randomPos, Quaternion.identity);
-        Destroy(newObject, lifeDuration);
+        return Vector3.zero;
+    }
+
+    void Update()
+    {
+        for (int i = spawnedObjects.Count - 1; i >= 0; i--)
+        {
+            GameObject obj = spawnedObjects[i];
+            if (Vector3.Distance(obj.transform.position, player.position) > destroyRange)
+            {
+                Destroy(obj);
+                spawnedObjects.RemoveAt(i);
+            }
+            else
+            {
+                obj.transform.Translate(flyDirection.normalized * moveSpeed * Time.deltaTime);
+            }
+        }
     }
 }
